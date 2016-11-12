06d220d047646d09ee2ff961420b486c1dcf7223
achallett <Aidan@Aidans-MacBook-Air.local>
2016-11-12 09:44:06 +0000

    Server now listens on port 3000 and 8080.

--- aidan/hello.js
+++ aidan/hello.js
@@ -8,6 +8,21 @@ http.createServer(function(request, response){
 	response.end();
 
 }).listen(8080);
-
 console.log("Listening on port 8080...")
+
+http.createServer(function(request, response){
+
+	response.writeHead(200);
+	response.write("Hello, this is port 3000");
+	console.log("Successful output on port 3000...")
+	response.end();
+
+}).listen(3000);
+console.log("Listening on port 3000...")
+
+setInterval(function(){ 
+	console.log("Nothing happened..."); 
+}, 2000);
+
+
 
